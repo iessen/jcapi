655485bf753714030987513ec98cd6552aa8113f
iessen <essen.wang@outlook.com>
2020-09-29 17:41:05 +0800

    add light
    add light

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jcapi",
-    version="0.0.2",
+    version="0.0.6",
     author="ewg",
     author_email="essen.wang@outlook.com",
     description="home-assistant skill api from jiachang.",
